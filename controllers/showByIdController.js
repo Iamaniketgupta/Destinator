676312bd3cb8ea72a73af21aa98b3029e36fd8fe
Aniket Gupta <aniketgupta.org@gmail.com>
2024-01-27 03:54:23 +0530

    feat: show by id controller to get complete details for the particular item

---
+++ controllers/showByIdController.js
@@ -0,0 +1,11 @@
+const Listings = require("../models/listingsSchema");
+
+const showByIdController = async (req) => {
+    let {id} = req.params;
+    try {
+        const details = await Listings.findById(id);
+        return details;
+    } catch (err) {}
+}
+
+module.exports = showByIdController;
