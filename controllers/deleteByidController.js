6650ff62c4a3cfa3d8fc533acf79b493be4caa7f
Aniket Gupta <aniketgupta.org@gmail.com>
2024-01-27 16:44:56 +0530

    feat : Delete Places Controller

---
+++ controllers/deleteByidController.js
@@ -0,0 +1,14 @@
+const Listings = require("../models/listingsSchema");
+
+const deleteByIdController = async (req,res) => {
+    let {id} = req.params;
+    try {
+        await Listings.findByIdAndDelete(id);
+        res.redirect("/");
+        
+    } catch (err) {}
+}
+
+
+
+module.exports =deleteByIdController;
