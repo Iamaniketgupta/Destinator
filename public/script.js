ad118799a073b79c73b930fb84f2be57d1e43626
Aniket Gupta <aniketgupta.org@gmail.com>
2024-01-28 05:11:02 +0530

    setup frontend for home page with responsive styling

--- public/script.js
+++ public/script.js
@@ -1,38 +1,31 @@
-
-const themeBtn = document.querySelector("#theme-changer i");
+// const themeBtn = document.querySelector("#theme-changer");
 const body = document.querySelector("body");
-
 const nav = document.querySelector("#nav-wrapper");
 const navIconBars = document.querySelector(".fa-bars");
 const navIconCross = document.querySelector("#cross");
+
 body.classList.add("light-theme"); // default theme
 
-//  THEME
+// THEME
+// themeBtn.addEventListener("click", (e) => {
+//     if (body.classList.contains("light-theme")) {
+//         body.classList.remove("light-theme");
+//         body.classList.add("dark-theme");
+//     } else {
+//         body.classList.remove("dark-theme");
+//         body.classList.add("light-theme");
+//     }
+//     themeBtn.classList.toggle("fa-moon");
+//     themeBtn.classList.toggle("fa-sun");
+// });
 
-themeBtn.addEventListener("click", (e) => {
+// NAVBAR
+navIconBars.addEventListener("click", toggleNav);
+navIconCross.addEventListener("click", toggleNav);
 
-    if (e.target.classList.contains("fa-sun")) {
-        body.classList.remove("dark-theme");
-        body.classList.add("light-theme");
-    }
-    else {
-        body.classList.add("dark-theme");
-        body.classList.remove("light-theme");
-    }
-    e.target.classList.toggle("fa-moon");
-    e.target.classList.toggle("fa-sun");
-
-});
-
-//  NAVBAR
-
-navIconBars.addEventListener("click", () => {
-    nav.classList.toggle("shownav")
-});
-
-navIconCross.addEventListener("click", () => {
-    nav.classList.toggle("shownav")
-});
+function toggleNav() {
+    nav.classList.toggle("shownav");
+}
 
 
 const deleteButtons = document.querySelectorAll(".deletePlace-btn");
