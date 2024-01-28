7003bd9eae835d90a8506c14e7de8619acdc366c
Aniket Gupta <aniketgupta.org@gmail.com>
2024-01-28 18:22:49 +0530

    set frontend and added responsive styling for user interface

--- public/script.js
+++ public/script.js
@@ -1,24 +1,7 @@
-// const themeBtn = document.querySelector("#theme-changer");
-const body = document.querySelector("body");
+
 const nav = document.querySelector("#nav-wrapper");
 const navIconBars = document.querySelector(".fa-bars");
 const navIconCross = document.querySelector("#cross");
-
-body.classList.add("light-theme"); // default theme
-
-// THEME
-// themeBtn.addEventListener("click", (e) => {
-//     if (body.classList.contains("light-theme")) {
-//         body.classList.remove("light-theme");
-//         body.classList.add("dark-theme");
-//     } else {
-//         body.classList.remove("dark-theme");
-//         body.classList.add("light-theme");
-//     }
-//     themeBtn.classList.toggle("fa-moon");
-//     themeBtn.classList.toggle("fa-sun");
-// });
-
 // NAVBAR
 navIconBars.addEventListener("click", toggleNav);
 navIconCross.addEventListener("click", toggleNav);
