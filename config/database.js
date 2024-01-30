d1de2010ff10d51ef513e0e217b2b68247e273e8
Aniket Gupta <aniketgupta.org@gmail.com>
2024-01-30 15:20:17 +0530

    added support of mongodb atlas

--- config/database.js
+++ config/database.js
@@ -1,17 +1,15 @@
-
 const mongoose = require("mongoose");
 const DB_URL = process.env.DB_URL;
 
-const dbConnect = async()=>{
-    try{
-
-        await mongoose.connect(`${DB_URL}/destinator_dev`)
-        .then(console.log("Connected to DB"));
-
-    }
-    catch(err){
-        console.log(err);
+const dbConnect = async () => {
+    try {
+        await mongoose.connect(DB_URL, {
+            dbName: 'destinator_dev'
+        });
+        console.log("Connected to MongoDB Atlas!");
+    } catch (err) {
+        console.error("Error connecting to MongoDB Atlas:", err);
     }
 }
 
-module.exports = dbConnect; 
+module.exports = dbConnect;
